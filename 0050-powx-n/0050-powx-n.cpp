f0d5832a69d9effa3cb66d6c85fa24839eeb7fcc
SHIVANSH SINGH CHAMBIYAL <shivanshsinghchambiyal088@gmail.com>
2026-06-23 16:42:22 +0530

    Time: 0 ms (100%), Space: 8.9 MB (6.4%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans= pow(x,n);
+       // long long  N=n;
+        //double ans=1;
+        //if(N<0)
+        //{
+        //     x=1/x;
+        //     N=-N;
+        // }
+        // for(long long  i=0;i<N;i++)
+        // {
+        //     ans*= x;
+        // }
+         
+
+         return ans;
+    }
+};
